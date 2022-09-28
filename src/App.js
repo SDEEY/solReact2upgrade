4022b04824b4e2c0e4c31b5d0b8958798828649e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-29 02:57:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = '0'
-const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/stonedhorses_pfp_1664137522032.jpeg'
-const Title = "Stoned Horses"
-const supply = 3333
+const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/necklessneighbors_pfp_1662012630188.gif'
+const Title = "Neckless Neighbors"
+const supply = 999
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
