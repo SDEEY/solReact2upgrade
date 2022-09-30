a030df4bd0be75258b885d987ef8134aee10a5e7
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-30 23:49:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '1.75 / 2'
-const image = 'https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/atrivians_pfp_1663012736300.png'
-const Title = "Creature Chronicles: The Atrivians"
-const supply = 4177
+const solAmount = '0'
+const image = 'https://media.discordapp.net/attachments/1002590795133636635/1022849155237953554/BATDOG.jpg?width=610&height=610'
+const Title = "Lost Dog Society"
+const supply = 6969
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
