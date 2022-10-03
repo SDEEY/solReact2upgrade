75ad83f32f6b40146441b7ffea14e78ea87f340c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-10-03 18:54:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0'
-const image = 'https://media.discordapp.net/attachments/1002590795133636635/1022849155237953554/BATDOG.jpg?width=610&height=610'
-const Title = "Lost Dog Society"
-const supply = 6969
+const solAmount = '2.5'
+const image = 'https://media.discordapp.net/attachments/966662928541573120/1026239660680298547/IMG_1105.jpg?width=491&height=491'
+const Title = "CyberBongs"
+const supply = 5472
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
