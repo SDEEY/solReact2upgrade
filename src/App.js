4ec2eebed5f8d608dbeb173e5afede5cef1bac3e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-11-23 00:58:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.65 / 0.95'
-const image = 'https://pbs.twimg.com/profile_images/1584560736990220288/yZeLlXSU_400x400.jpg'
-const Title = "𝐍𝐎𝐙𝐎𝐌𝐈"
-const supply = 5555
+const solAmount = '1'
+const image = 'https://pbs.twimg.com/profile_images/1575187558224564224/YimIV_nf_400x400.jpg'
+const Title = "DEFI SOCIETY"
+const supply = 2172
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
