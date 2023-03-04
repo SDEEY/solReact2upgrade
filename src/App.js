1f17c905f6dcc0bc74c00795a3e17c0cd489b41c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-04 20:15:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.125'
-const image = 'https://pbs.twimg.com/profile_images/1627673337832022018/1xKV6aH__400x400.jpg'
-const Title = "THE FLOCK"
-const supply = 1271
+const solAmount = '0.075'
+const image = 'https://pbs.twimg.com/profile_images/1629320081347051523/y9bqKyiY_400x400.jpg'
+const Title = "OKAY B00BS"
+const supply = 3250
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
