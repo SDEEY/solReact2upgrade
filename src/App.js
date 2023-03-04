0622c1f369014ce5f05c30f16f9001f68f764557
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-04 22:35:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.075'
-const image = 'https://pbs.twimg.com/profile_images/1629320081347051523/y9bqKyiY_400x400.jpg'
-const Title = "OKAY B00BS"
-const supply = 3250
+const solAmount = '0.7'
+const image = 'https://pbs.twimg.com/profile_images/1615236870421762048/-jl8hGow_400x400.jpg'
+const Title = "AICATZ"
+const supply = 18
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
