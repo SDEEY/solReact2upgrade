e08e319e6d5ca9755fdd811ad417af6c771c56b2
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-12 21:20:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import imgTwitter from './icons8-twitter-50.png'
 const solAmount = '0.22'
 const image = 'https://pbs.twimg.com/profile_images/1625908990357164034/CBfvNvZ5_400x400.jpg'
 const Title = "MoonSeeker DAO"
-const supply = 851
+const supply = 40
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
