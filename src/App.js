7b1868bcd18862623dad27827ce15dd892ff2df1
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-13 22:42:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = '0'
-const image = 'https://pbs.twimg.com/media/Fqkx6iNWYAA0U4O?format=jpg&name=4096x4096'
-const Title = "Unic Gods"
-const supply = 777
+const image = 'https://pbs.twimg.com/profile_images/1596735522940280833/_xQ6Ur-G_400x400.jpg'
+const Title = "Chipmunk Man"
+const supply = 177
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
