13bda8d5e66e6b11b0368cddd2d367edf8d18df2
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-13 21:47:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1632018703825993729/RBGYSzZy_400x400.jpg'
+const image = 'https://pbs.twimg.com/media/Fqkx6iNWYAA0U4O?format=jpg&name=4096x4096'
 const Title = "Unic Gods"
 const supply = 777
 
