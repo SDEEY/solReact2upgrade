405756434555fbb828b92536e1d7b72ee37c672b
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-17 16:42:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '2'
-const image = 'https://pbs.twimg.com/profile_images/1627510118786146305/EdmhLaS7_400x400.jpg'
-const Title = "SolDrop"
-const supply = 170
+const solAmount = '1'
+const image = 'https://pbs.twimg.com/profile_images/1527414053668548609/v6EfPrdr_400x400.jpg'
+const Title = "alphaapeelite"
+const supply = 544
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
