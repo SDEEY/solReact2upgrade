d7099968c73d4dcfc8bf4350458196341cc38cd2
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-31 19:45:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '1.2'
-const image = 'https://pbs.twimg.com/profile_images/1593730340853633025/hWrzy2rK_400x400.jpg'
-const Title = 'Icebreaker'
-const supply = 282
+const solAmount = '0.03'
+const image = 'https://pbs.twimg.com/profile_images/1639524045572980737/vYbipFSI_400x400.jpg'
+const Title = 'Kings Of Da Rug'
+const supply = 3877
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
