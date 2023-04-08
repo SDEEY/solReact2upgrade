94168b8428b2ad8f164d7e311cb42253263ca481
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-08 21:16:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0 / 0.0777'
-const image = 'https://pbs.twimg.com/profile_images/1643089421359562758/xcKmBuwK_400x400.jpg'
-const Title = 'Bloodpack'
-const supply = 1744
+const solAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1636294563206668289/tJW6HmeM_400x400.jpg'
+const Title = 'Immortal Apes'
+const supply = 3780
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
