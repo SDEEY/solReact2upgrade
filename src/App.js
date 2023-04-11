a956c9e6bdbae932104c80c8357254259857edc4
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-11 20:38:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0
-const image = 'https://pbs.twimg.com/profile_images/1633187685459173376/Tub1L12B_400x400.jpg'
-const Title = 'secret monkey'
-const supply = 222
+const solAmount = 0.33
+const image = 'https://pbs.twimg.com/profile_images/1617306244901122049/JL5DciEm_400x400.jpg'
+const Title = 'Updog'
+const supply = 1480
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
