b038d50fd651bda8599480497ee06b3ce999dbea
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-24 12:55:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.3
-const image = 'https://pbs.twimg.com/profile_images/1648282825982877697/fLIsFHfx_400x400.jpg'
-const Title = 'Block Bounty'
-const supply = 333
+const solAmount = 0.2
+const image = 'https://pbs.twimg.com/profile_images/1614903751776550914/U-pioptY_400x400.jpg'
+const Title = 'LIONS'
+const supply = 4000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
