09af24d7e5b5cf7e2541452c14b4c98f70304756
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-26 21:07:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.69'
-const image = 'https://pbs.twimg.com/profile_images/1683729275101368320/DJ3GnVR__400x400.jpg'
-const Title = 'Froogies'
-const supply = 29
+const solAmount = "0"
+const image = 'https://pbs.twimg.com/profile_images/1681974918844665858/WBTc_Jm9_400x400.jpg'
+const Title = 'BYTE CITY'
+const supply = 1197
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
